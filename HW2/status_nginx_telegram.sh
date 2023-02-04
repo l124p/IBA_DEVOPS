675f20221251bbf67b53551ae51187ee8219226c
Andry <andry.peshko@gmail.com>
2023-02-04 19:48:14 +0300

    add sh

---
+++ HW2/status_nginx_telegram.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TOKEN="6095982572:AAFgg6_e9NRzgEGRI-l_yAyzIp9NUVexkps"
+ID="361939818"
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+pattert="200 OK"
+
+while true
+do
+  result=$(curl -Iks head 1 127.0.0.1 | head -1 | grep -o "200 OK")
+  if [[ $result != "$pattert" ]]
+  then
+    curl -s -X POST $URL -d chat_id=$ID -d text="NGINX is not running"
+    systemctl start nginx
+  fi
+  pause 60
+done
